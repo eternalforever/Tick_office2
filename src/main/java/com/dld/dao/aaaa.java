33b710c63aebb88ce0c01863777b96a0200233cd
wxb <wxb_1214@plig.com>
2018-10-15 22:33:36 +0800

    aaaa

---
+++ src/main/java/com/dld/dao/aaaa.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: aaaa
+ * Author:   邦哥的电脑
+ * Date:     2018-10-15 22:31
+ * Description: aa
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.dld.dao;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈aa〉
+ *
+ * @author 邦哥的电脑
+ * @create 2018-10-15
+ * @since 1.0.0
+ */
+public interface aaaa {
+
+}
+
+
