a28feb8b4141ab15384499600cb0952b91ff082d
wxb <wxb_1214@plig.com>
2018-10-15 22:34:46 +0800

    aaaa

---
+++ src/main/java/com/dld/dao/afcasda.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: afcasda
+ * Author:   邦哥的电脑
+ * Date:     2018-10-15 22:34
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.dld.dao;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author 邦哥的电脑
+ * @create 2018-10-15
+ * @since 1.0.0
+ */
+public class afcasda {
+
+}
+
+
